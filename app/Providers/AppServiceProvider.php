3582021865f6c785bd7acaf639861b3e213378c3
Thomas James Gale <t.gale2547@student.leedsbeckett.ac.uk>
2019-11-26 00:20:47 +0000

    Just finished episode 5 of the eloquent relationships

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
          \URL::forceScheme('https');
+         Relation::morphMap([
+         
+         'series' => 'App\Series',
+         'collection' => 'App\Collection'
+         ]);
     }
 
 
---
+++ app/Series.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Series extends Model
+{
+    
+    public function videos() {
+ 
+ return $this->morphMany(Video::class, 'watchable');
+ 
+}
+
+
+}
