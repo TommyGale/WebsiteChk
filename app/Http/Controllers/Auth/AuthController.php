259be79d89c4eb6e578d140f2f93961e20fc7d52
Thomas James Gale <t.gale2547@student.leedsbeckett.ac.uk>
2019-11-27 09:46:30 +0000

    working git user just need to change the laravel server everytime

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -18,10 +18,33 @@ class AuthController extends Controller
     
     public function handleProviderCallBack() {
         
-        $githubUser = Socialite::driver('github')->user();
         
-        dd($githubUser);
+        $user = $this->findOrCreateGitHubUser(
+        $githubUser = Socialite::driver('github')->user()
+        );
         
+        auth()->login($user);
+        
+         //dd($githubUser);
+        
+        return redirect ('/');
+    }
+    
+    protected function findOrCreateGitHubUser($githubUser){
+        
+        $user = User::firstOrNew(['github_id' => $githubUser->id]);
+        
+        //if($user->exists){
+          //  return $user;
+        //} 
+        
+        $user->fill([
+        
+        'name' => $githubUser->nickname,
+        'email' => $githubUser->email
+        ])->save();
+        
+        return $user;
     }
 }
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('affiliation_id')->default(0);
+            $table->unsignedInteger('affiliation_id')->nullable();
+            $table->integer('github_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
